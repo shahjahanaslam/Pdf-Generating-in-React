a059ca2f775a4273b9b1c0313444e73c889c4c18
unknown <shahjahanmuhammadaslam@gmail.com>
2020-05-11 14:21:51 +0500

    11-May-2020

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import {saveAs} from 'file-saver';
 
-function App() {
+
+class App extends Component{
+  constructor(props){
+  super(props)
+
+  this.state={
+
+    name: '',
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+
+}
+
+} 
+
+handleChange =({target : {value, name}}) => this.setState({
+  [name]: value 
+})
+
+createAndDownloadPdf =() =>{
+  axios.post('/create-pdf', this.state)
+  .then(()=> axios.get('fetch-pdf', {responseType: 'blob' }))
+   .then((res)=> {
+     const pdfBlob = new Blob([res.data], {type: 'application/pdf'})
+     saveAs(pdfBlob, `${this.state.receiptId}.pdf`);
+
+   })
+}
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="text"  placeholder="Name" name="name"  onChange={this.handleChange} />
+    <input type="number"  placeholder="Receipt ID" name="receiptId"  onChange={this.handleChange} />
+    <input type="number"  placeholder="Price 1" name="price1"  onChange={this.handleChange} />
+    <input type="number"  placeholder="Price 2"  name="price2" onChange={this.handleChange} />
+    <button onClick={this.createAndDownloadPdf}>
+ 
+     Download pDf
+    </button>
+
     </div>
-  );
+
+    
+   
+    );
+}
+
 }
 
 export default App;
